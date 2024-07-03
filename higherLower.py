a5bc00dbfa97978dd6d4a6931de810fc00ce3374
Jimena Romo Cuevas <105473411+omgymna@users.noreply.github.com>
2024-07-03 09:05:39 -0400

    Create higherLower.py

---
+++ higherLower.py
@@ -0,0 +1,37 @@
+import random
+from art import hl_logo, vs_art
+from game_data import data
+from replit import clear
+
+#This is a guessing game to guess who has more instagram followers. Game continues until wrong answer is given.
+
+
+should_continue = True
+score = 0
+hl_logo()
+choice_a = random.choice(data)
+
+while should_continue:
+  
+  choice_b = random.choice(data)
+  if choice_b == choice_a:
+    choice_b = random.choice(data)
+
+  print(f"Compare A: {choice_a['name']}, a {choice_a['description']}, from {choice_a['country']}")
+  vs_art() 
+  print(f"Against B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}")
+  
+
+  guess = input("Who has more followers? Type 'A' or 'B': ").upper()
+  count_a = choice_a['follower_count']
+  count_b = choice_b['follower_count']
+  
+  if (guess == 'A' and count_a > count_b) or (guess == 'B' and count_a < count_b):
+    score += 1
+    clear()
+    hl_logo()
+    print(f"You're Right! Score is: {score}")
+    choice_a = choice_b
+  else:
+    print(f"Sorry, that's wrong. Final score: {score}")
+    should_continue = False
